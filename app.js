b91d6ac52cc28081e757bc7eeae699f651b3926e
arshiya <arshkhan1002@gmail.com>
2024-01-27 01:58:26 -0500

    episode1

---
+++ app.js
@@ -0,0 +1,25 @@
+const parent = [React.createElement("div",{id: "parent"},[
+    React.createElement("div",{id: "child"}, [
+    React.createElement("h1",{}, "H1 tag"), 
+    React.createElement("h2",{}, "H2 tag")
+ ]),
+    React.createElement("div",{id: "child2"}, [
+    React.createElement("h1",{}, "H1 tag"), 
+    React.createElement("h2",{}, "H2 tag")
+ ])
+]),
+React.createElement("div",{id: "parent2"},[
+    React.createElement("div",{id: "child"}, [
+    React.createElement("h1",{}, "H1 tag"), 
+    React.createElement("h2",{}, "H2 tag")
+ ]),
+    React.createElement("div",{id: "child2"}, [
+    React.createElement("h1",{}, "H1 tag"), 
+    React.createElement("h2",{}, "H2 tag")
+ ])
+])];
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
+
+
